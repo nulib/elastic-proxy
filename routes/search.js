bc9d4454debf4629968986c0b28dffd6652b9aee
Michael Klein <mbklein@gmail.com>
2019-05-16 12:48:07 -0500

    Fix OPTIONS/HEAD requests for pings to `/search/`

--- routes/search.js
+++ routes/search.js
@@ -2,10 +2,23 @@ const ESProxy = require('../lib/es-proxy');
 const express = require('express');
 const router  = express.Router();
 
-function passthru(req, res, next) {
+function passthru(req, res, _next) {
   let proxy = new ESProxy(req);
   proxy.passthru(res);
 }
+
+function ping(req, res, _next) {
+  res.set('Content-Type', 'application/json');
+  res.postProcess();
+  if (req.method !== 'OPTIONS') {
+    res.send('{"ping":"OK"}');
+  }
+  res.send();
+}
+
+router.get('/', ping)
+router.head('/', ping);
+router.options('/', ping);
 
 router.head('/*', passthru);
 router.options('/*', passthru);
